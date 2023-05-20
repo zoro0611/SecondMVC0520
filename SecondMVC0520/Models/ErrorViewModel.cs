3461b0c1b6be16e8bea26ee9c41f838be98fec1b
Hsu Leo <zoro061181249@gmail.com>
2023-05-20 23:37:24 +0800

    加入專案檔案。

---
+++ SecondMVC0520/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SecondMVC0520.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
