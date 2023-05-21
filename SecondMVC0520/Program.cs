09890923c13d76a7bf5295beb8693c341031e025
Hsu Leo <zoro061181249@gmail.com>
2023-05-21 11:12:53 +0800

    將conn string改存user secrets

--- SecondMVC0520/Program.cs
+++ SecondMVC0520/Program.cs
@@ -3,12 +3,24 @@ using Microsoft.Extensions.Options;
 using SecondMVC0520.DBModels;
 using SecondMVC0520.Repos;
 using SecondMVC0520.Service;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Azure.Identity;
 
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+//開發環境使用user secret取得連線字串，若正式環境使用azure app service的user secret，則需在azure app service取得connecion string
+builder.Configuration["ConnectionStrings:SecondMvcConnection"] = builder.Configuration["ConnectionStrings:SecondMvcConnection"];
+
+
+
+
 
 builder.Services.AddDbContext<SecondMVC0520_dbContext>(
     options =>
